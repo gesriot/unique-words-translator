da8c4574b04cc7766f8102cebfc3fcf8718f8960
Alexander Shabalin <gessman1618@gmail.com>
2023-11-08 01:30:26 +0300

    Initial commit

---
+++ alphabetical-sort.py
@@ -0,0 +1,51 @@
+import nltk
+import sys
+from nltk.corpus import wordnet
+from nltk.tokenize import word_tokenize
+from deep_translator import GoogleTranslator
+
+
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
+
+def get_wordnet_pos(treebank_tag):
+    if treebank_tag.startswith('J'):
+        return wordnet.ADJ
+    elif treebank_tag.startswith('V'):
+        return wordnet.VERB
+    elif treebank_tag.startswith('N'):
+        return wordnet.NOUN
+    elif treebank_tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return ''
+
+def process_file(filename):
+    with open(filename, 'r') as f:
+        text = f.read()
+
+    words = word_tokenize(text)
+    pos_tags = nltk.pos_tag(words)
+
+    unique_words = set()
+
+    for word, pos in pos_tags:
+        wordnet_pos = get_wordnet_pos(pos)
+        if wordnet_pos in {wordnet.NOUN, wordnet.VERB, wordnet.ADJ} and word.isalpha() and len(word) >= 3 and word.encode('ascii', 'ignore').decode() == word:
+            unique_words.add(word)
+
+    return sorted(list(unique_words))
+
+filename = sys.argv[1]
+unique_words = process_file(filename)
+
+translator = GoogleTranslator(source='auto', target='russian')
+
+with open('en.txt', 'w') as en_file, open('ru.txt', 'w') as ru_file:
+    for word in unique_words:
+        en_file.write(word + '\n')
+        translation = translator.translate(word)
+        ru_file.write(translation + '\n')
+
+print("Done")
